534e4865d310d0bf5c6864805839bf56a0ffa077
Alessandro Lodigiani <46444351+sylar9696@users.noreply.github.com>
2022-06-07 13:13:24 +0200

    prima parte auth

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('guest.home');
+// });
+
+Auth::routes();
+
+// localhost:8000/admin
+Route::middleware('auth')
+->prefix('admin') //legato alla uri
+->name('admin.')
+->namespace('Admin')
+->group( function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
 });
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+} )->where("any", ".*");
