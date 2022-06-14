a467335b5753bd0b678e22155ee4e4c44e4361b7
Alessandro Lodigiani <46444351+sylar9696@users.noreply.github.com>
2022-06-14 10:54:43 +0200

    file upload

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -8,6 +8,7 @@ use App\Models\Post;
 use App\Models\Category;
 use App\Models\Tag;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -46,9 +47,14 @@ class PostController extends Controller
     {
         $data = $request->all();
 
-        // dd($data);
+        //dd($data);
 
         $post = new Post();
+
+        if(array_key_exists('image', $data)){
+            $image_url = Storage::put('post_images', $data['image'] );
+            $data['image'] = $image_url;
+        }
 
         $post->fill($data);
         $post->slug = Str::slug($post->title, '-');
@@ -102,6 +108,16 @@ class PostController extends Controller
         $data = $request->all();
         // dd($data);
         $post['slug'] = Str::slug( $request->title , '-');
+
+
+        if(array_key_exists('image', $data)){
+            if( $post->image ) Storage::delete($post->image);
+
+            $image_url = Storage::put('post_images', $data['image'] );
+            $data['image'] = $image_url;
+        }
+
+
         $post->update($data);
 
         // if ( !array_key_exists( 'tags', $data ) )  $post->tags()->detach();
